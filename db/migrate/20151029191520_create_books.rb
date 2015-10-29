e1478680a038f7fe4eb4af57880b58b7b3a19577
segerard <sarah-gerard@uiowa.edu>
2015-10-29 19:44:00 +0000

    set up movies routes, users routes, and books database. Also added views for books.

---
+++ db/migrate/20151029191520_create_books.rb
@@ -0,0 +1,13 @@
+class CreateBooks < ActiveRecord::Migration
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.string :author
+      t.string :isbn
+      t.string :quality
+      t.string :price
+      t.text :description
+      t.string :seller
+    end
+  end
+end
