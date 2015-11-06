49e64f3066738bb38168bad1cbe082068b678e55
segerard <sarah-gerard@uiowa.edu>
2015-11-06 19:42:00 +0000

    model tests

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -4,25 +4,36 @@ require 'rails_helper'
 
 describe Book do
   describe 'searching OpenLibrary by isbn' do
-    before :each do
-      @fake_book = double('book')
-      allow(@fake_book).to receive(:title).and_return('Book Title')
-      allow(@fake_book).to receive(:authors).and_return(["name" => "Sarah"])
-      allow(@fake_book).to receive(:thumbnail_url).and_return('image.gif')
-    end
     context 'with found isbn' do
+      before :each do
+        @fake_book = double('data')
+        allow(@fake_book).to receive(:title).and_return('Book Title')
+        allow(@fake_book).to receive(:authors).and_return(["name" => "Sarah"])
+        allow(@fake_book).to receive(:thumbnail_url).and_return('image.gif')
+        allow(Openlibrary::View).to receive(:find_by_isbn).with('123456789').and_return(@fake_book)
+        allow(Openlibrary::Data).to receive(:find_by_isbn).with('123456789').and_return(@fake_book)
+      end
       it 'should call Openlibrary::View with isbn' do
-        allow(Openlibrary::View).to receive(:find_by_isbn).with('123456789').and_return([@fake_book])
         expect(Openlibrary::View).to receive(:find_by_isbn).with('123456789')
-        Book.open_lib_find_by_isbn('123456789')
+        Book.open_lib_find_book('123456789')
       end
       it 'should call Openlibrary::Data with isbn' do
-        allow(Openlibrary::Data).to receive(:find_by_isbn).with('123456789').and_return([@fake_book])
         expect(Openlibrary::Data).to receive(:find_by_isbn).with('123456789')
-        Book.open_lib_find_by_isbn('123456789')
+        Book.open_lib_find_book('123456789')
+      end
+      it 'it should assign values to book has' do
+#        expect(assigns(:book)).to eq({title:'Book Title', author:"Sarah", isbn:"123456789", image:'image.gif', price:"", quality:""}) 
       end
     end
     context 'isbn not found' do
+      before :each do
+        @fake_book = double('data')
+        allow(Openlibrary::Data).to receive(:find_by_isbn).with('')
+      end
+      it 'it should assign empty hash to book' do
+        Book.open_lib_find_book('')
+#        expect(assigns(:book)).to eq({}) 
+      end
     end
   end
 end
