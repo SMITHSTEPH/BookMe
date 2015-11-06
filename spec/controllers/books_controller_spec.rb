e6ec46458c62a6c5465d68c67c059e6dd5c2aff5
segerard <sarah-gerard@uiowa.edu>
2015-11-06 04:41:10 +0000

    tests added

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -18,8 +18,25 @@ describe BooksController do
       it 'should select the Search Results template for rendering' do
         expect(response).to render_template('new')
       end
-      it 'should make the TMDb search results available to that template' do
+      it 'it should redirect to new page' do
         expect(assigns(:book)).to eq(@fake_results)
+      end
+    end
+    describe 'after no matching books search' do
+      before :each do
+        allow(Book).to receive(:open_lib_find_book).and_return({})
+      end  
+      it 'should call model method that performs Tmdb search' do
+        expect(Book).to receive(:open_lib_find_book)
+        post :search_open_lib, {:isbn => '123456789'}
+      end
+      it 'it should redirect to new page' do
+        post :search_open_lib, {:isbn => '123456789'}
+        expect(response).to render_template('new')
+      end
+      it 'should display flash saying no matches found' do
+        post :search_open_lib, {:isbn => '123456789'}
+        expect(flash[:warning]).to eq("Book not found in database!")
       end
     end
   end
