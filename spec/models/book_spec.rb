42005db8acd40cee6dcaa663471cb3856a96a323
segerard <sarah-gerard@uiowa.edu>
2015-11-06 20:05:32 +0000

    model tests passing

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -22,7 +22,8 @@ describe Book do
         Book.open_lib_find_book('123456789')
       end
       it 'it should assign values to book has' do
-#        expect(assigns(:book)).to eq({title:'Book Title', author:"Sarah", isbn:"123456789", image:'image.gif', price:"", quality:""}) 
+        result = Book.open_lib_find_book('123456789')
+        expect(result).to eq({title:'Book Title', author:"Sarah", isbn:"123456789", image:'image.gif', price:"", quality:""}) 
       end
     end
     context 'isbn not found' do
@@ -31,8 +32,23 @@ describe Book do
         allow(Openlibrary::Data).to receive(:find_by_isbn).with('')
       end
       it 'it should assign empty hash to book' do
-        Book.open_lib_find_book('')
-#        expect(assigns(:book)).to eq({}) 
+        result = Book.open_lib_find_book('')
+        expect(result).to eq({}) 
+      end
+    end
+    context 'no image found' do
+      before :each do
+        @fake_book = double('data')
+        allow(@fake_book).to receive(:title).and_return('Book Title')
+        allow(@fake_book).to receive(:authors).and_return(["name" => "Sarah"])
+        allow(@fake_book).to receive(:thumbnail_url).and_return(nil)
+        allow(Openlibrary::View).to receive(:find_by_isbn).with('123456789').and_return(@fake_book)
+        allow(Openlibrary::Data).to receive(:find_by_isbn).with('123456789').and_return(@fake_book)
+      end
+      it 'should assign template image if no image is entered' do
+        result = Book.open_lib_find_book('123456789')
+        expect(result).to eq({title:'Book Title', author:"Sarah", isbn:"123456789", image:'nobook.gif', price:"", quality:""}) 
+      
       end
     end
   end
