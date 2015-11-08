ed0d46a700c7d880e255f885c4fb22a83f9195c2
dnham <blake-dunham@uiowa.edu>
2015-11-08 00:29:54 +0000

    User Spec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,2 +1,12 @@
 require 'spec_helper'
 require 'rails_helper'
+
+describe UserController do
+  describe 'creating account' do
+    context 'Required fields are filled in' do
+      before :each do
+        @fake_user = {"first_name" => "Blake", "last_name" => "Dunham", "user_id" => "dnham", "email" => "b@b.com", "password" => "password", "condirmPassword" => "password"}
+      end
+    end
+  end
+end
