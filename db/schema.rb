1e0d7c3d035a794dd95e91508524d20ae1cf2476
dnham <blake-dunham@uiowa.edu>
2015-11-20 05:36:25 +0000

    Added search by course and department

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,6 @@
 ActiveRecord::Schema.define(version: 20151111221419) do
 
   create_table "books", force: :cascade do |t|
-    t.integer  "user_id"
     t.string   "title"
     t.string   "author"
     t.string   "isbn"
@@ -22,14 +21,13 @@ ActiveRecord::Schema.define(version: 20151111221419) do
     t.string   "image"
     t.string   "price"
     t.text     "description"
+    t.string   "seller"
     t.string   "department"
     t.string   "course"
     t.string   "auction_start_price"
     t.datetime "auction_time"
     t.string   "time_left"
   end
-
-  add_index "books", ["user_id"], name: "index_books_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
