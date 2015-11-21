f368bcd1b2207d9d8041ed2e7257facad16febc7
smithsteph <stephanie-k-smith@uiowa.edu>
2015-11-21 03:32:09 +0000

    blah

--- app/assets/javascripts/add_keyword.js
+++ app/assets/javascripts/add_keyword.js
@@ -1,4 +1,4 @@
-var text_template="<div class='component' id='keyword_div_NUM'><div class='form-group'><div class='col-md-4'>"
+var text_template="</br><div class='component' id='keyword_div_NUM'><div class='form-group'><div class='col-md-4'>"
 +"<input type='text' name='book[keyword[NUM]]' id='book[keyword][NUM]' class='keywords form-control'>"
 +"</div></div></div>"+"</br></br>"
 
@@ -19,15 +19,3 @@ function click_handler(){
     //alert("Hello World")
     console.log("went in click");
 }
-
-/*$(function() {
-    var template = "<textarea name='quiz[content][INDEX]'></textarea>",
-        index = $('textarea').length,
-        compiled_template;
-    
-    $('#js-add-question-row').click(function() {
-        compiled_textarea = $(template.replace("INDEX", index));
-        $('#someform').append(compiled_textarea);
-        index = index + 1;
-    });
-});*/
