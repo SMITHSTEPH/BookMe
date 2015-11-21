4f8a594782d3d21814ca99264612ace0f07ecfea
smithsteph <stephanie-k-smith@uiowa.edu>
2015-11-21 02:44:43 +0000

    trying to fix bug on heroku

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -131,10 +131,10 @@ class BooksController < ApplicationController
     @keywords = []
     puts "is empty?"
     puts @keywords.empty?
-    if Tag.where("book_id=="+@book.id.to_s).exists? #getting the keywords if there are an
+    if Tag.find_by(book_id: @book.id.to_s).exists? #getting the keywords if there are an
       puts "id is: " +  @book.id.to_s
       Tag.find_each  do |keyword|
-        if keyword.book_id == @book.id
+        if keyword.book_id.equals @book.id
           puts "in if keyword matches"
            @keywords << keyword
         end
