e5416a302b11a675a5f0523e4a7aab32f86fcc9f
segerard <sarah-gerard@uiowa.edu>
2015-12-09 05:13:56 +0000

    added images to seeded books

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,10 +15,10 @@ users.each do |user|
   User.create!(user)
 end
 
-books = [{:title => 'Algorithm Design', :author => 'Kleinberg Tardos', :isbn => '9788131703106', :quality => 'great', :price => '50.00', :image => 'nobook.gif', :bid_price=>'3.00',:auction_start_price=>'3.00', :auction_time=> DateTime.strptime("01/01/2016 8:00", "%m/%d/%Y %H:%M")},
-          {:title => 'Medical Imaging', :author => 'Sonka Fitzpatrick', :isbn => '0819436224', :quality => 'fair', :price => '60.00', :image => 'nobook.gif', :bid_price=>'1.00',:auction_start_price=>'1.00', :auction_time=> DateTime.strptime("01/01/2016 8:00", "%m/%d/%Y %H:%M")},
-          {:title => 'Image Processing, Analysis, and Machine Vision', :author => 'Sonka Hlavac Boyle', :isbn => '0534953930', :quality => 'great', :price => '55.00', :image => 'nobook.gif', :bid_price=>'4.00', :auction_time=> DateTime.strptime("01/01/2016 8:00", "%m/%d/%Y %H:%M")},
-          {:title => 'A Guide to Latex', :author => 'Kopka Daly', :isbn => '0201568896', :quality => 'great', :price => '40.00',:image => 'nobook.gif', :bid_price=>'2.00', :auction_start_price=>'2.00', :auction_time=> DateTime.strptime("01/01/2016 8:00", "%m/%d/%Y %H:%M")},
+books = [{:title => 'Algorithm Design', :author => 'Kleinberg Tardos', :isbn => '9788131703106', :quality => 'great', :price => '50.00', :image => 'algorithm.jpg', :bid_price=>'20.00',:auction_start_price=>'20.00', :auction_time=> DateTime.strptime("01/01/2016 8:00", "%m/%d/%Y %H:%M")},
+          {:title => 'Medical Imaging', :author => 'Sonka Fitzpatrick', :isbn => '0819436224', :quality => 'fair', :price => '60.00', :image => 'medicalimaging.jpg', :bid_price=>'25.00',:auction_start_price=>'25.00', :auction_time=> DateTime.strptime("01/01/2016 8:00", "%m/%d/%Y %H:%M")},
+          {:title => 'Image Processing, Analysis, and Machine Vision', :author => 'Sonka Hlavac Boyle', :isbn => '0534953930', :quality => 'great', :price => '55.00', :image => 'imageprocessing.jpg', :bid_price=>'15.00', :auction_start_price =>'15.00', :auction_time=> DateTime.strptime("01/01/2016 8:00", "%m/%d/%Y %H:%M")},
+          {:title => 'A Guide to Latex', :author => 'Kopka Daly', :isbn => '0201568896', :quality => 'great', :price => '40.00',:image => 'latex.jpg', :bid_price=>'10.00', :auction_start_price=>'10.00', :auction_time=> DateTime.strptime("01/01/2016 8:00", "%m/%d/%Y %H:%M")},
 
 ]
 
